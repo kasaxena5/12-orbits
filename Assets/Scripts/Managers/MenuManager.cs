fc5c4530fef9df29aff07a7c47865e0d8695a05a
Kartikeya Saxena <kasaxena@microsoft.com>
2023-04-30 19:47:16 +0530

    Menu Setup Ready

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneLoader.Instance.currentScene = "GameScene";
+        SceneLoader.Instance.StartScene();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
